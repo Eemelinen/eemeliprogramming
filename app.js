6cfd500eb3a082d7577aeb674394c8591c5ddfda
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-10-05 16:03:49 +0200

    Responsive header

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ const adjustY = 0;
 
 let particleArray = [];
 
+let sizeModifier = 1;
+
+function calcSize() {
+  if (window.innerWidth > 1400) {
+    sizeModifier = 1;
+  } else if (window.innerWidth < 1400 && window.innerWidth > 800) {
+    sizeModifier = 1.5;
+  } else if (window.innerWidth < 800 && window.innerWidth > 600) {
+    sizeModifier = 2;
+  } else if (window.innerWidth < 600 && window.innerWidth > 400) {
+    sizeModifier = 3.2;
+  }
+}
+
 let mouse = {
-  x: null,
-  y: null,
+  x: 2000,
+  y: 2000,
   // radius: (canvas.height / 130) * (canvas.width / 130),
-  radius: 100,
+  radius: 100 / sizeModifier,
 };
 
 window.addEventListener('mousemove', function(event) {
@@ -20,7 +34,7 @@ window.addEventListener('mousemove', function(event) {
 });
 
 ctx.fillStyle = 'white';
-ctx.font = '20px Verdana';
+ctx.font = `${20 / sizeModifier}px Verdana`;
 ctx.fillText('Developer', 0, 30);
 const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
@@ -28,7 +42,7 @@ class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 3;
+    this.size = 3 / sizeModifier;
     this.baseX = this.x;
     this.baseY = this.y;
     // Density affects the movement speed of parcticles making them more natural
@@ -80,10 +94,10 @@ function connect() {
       let distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < 25) {
-        opacityValue = 1 - (distance / 50);
+        opacityValue = 1 - (distance / (50 / sizeModifier));
         ctx.strokeStyle = `rgba(223, 83, 83, ${opacityValue})`
         // ctx.strokeStyle = 'pink';
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 2 / sizeModifier;
         ctx.beginPath();
         ctx.moveTo(particleArray[a].x, particleArray[a].y);
         ctx.lineTo(particleArray[b].x, particleArray[b].y);
@@ -95,12 +109,13 @@ function connect() {
 
 function init() {
   particleArray = [];
+  calcSize();
   for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
     for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
       if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
         let positionX = x + adjustX;
         let positionY = y + adjustY;
-        particleArray.push(new Particle(positionX * 12, positionY * 12));
+        particleArray.push(new Particle(positionX * (12 / sizeModifier), positionY * (12 / sizeModifier)));
       }
     }
   }
@@ -118,6 +133,14 @@ function animate() {
 
 init();
 animate();
+
+window.addEventListener('resize',
+  function() {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    init();
+  }
+);
 
 window.addEventListener('mouseout',
   function() {
